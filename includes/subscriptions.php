d744e7f5c76ba53acd39ac45dfdae36670759f02
Luke Stokes <luke.stokes@gmail.com>
2017-05-08 23:19:35 -0500

    More improvements to the subscription CSV export.

--- includes/subscriptions.php
+++ includes/subscriptions.php
@@ -1,22 +1,22 @@
 <?php
 
-function getSubscriptionsAsCSV($fc, $subs_uri, $offset = 0,$csv = '') {
-    $header = '"customer_id","email","start_date","next_transaction_date","end_date","frequency","past_due_amount","';
-    $header .= '"billing_first_name","billing_last_name","billing_company","billing_address1","billing_city","billing_region","billing_postal_code","billing_country"';
-    $header .= '"item_name","price","quantity","item_options"';
+function printSubscriptionsAsCSV($fc, $subs_uri, $offset = 0) {
+    $header = '"sub_token_url","customer_id","email","start_date","next_transaction_date","end_date","frequency","past_due_amount",';
+    $header .= '"billing_first_name","billing_last_name","billing_company","billing_address1","billing_city","billing_region","billing_postal_code","billing_country",';
+    $header .= '"item_name","price","quantity","item_options",';
     $header .= '"shipping_first_name","shipping_last_name","shipping_company","shipping_address1","shipping_city","shipping_region","shipping_postal_code","shipping_country"';
     $header .= "\n";
     $csv_header = '';
     $csv_detail = '';
-    if ($csv == '') {
-        $csv .= $header;
+    if ($offset == 0) {
+        print $header;
     }
     $limit = 300;
     $params = array(
-        'zoom' => 'customer:default_billing_address,last_transaction:shipments,transaction_template,transaction_template:items,transaction_template:item_options',
+        'zoom' => 'customer:default_billing_address,last_transaction:shipments,transaction_template,transaction_template:items,transaction_template:items:item_options',
         'is_active' => 1,
-        'offset' => $offeset,
-        'limit' => $limit
+        'offset' => $offset,
+        'limit' => $limit,
         );
     $result = $fc->get($subs_uri,$params);
     foreach($result['_embedded']['fx:subscriptions'] as $subscription) {
@@ -28,7 +28,8 @@ function getSubscriptionsAsCSV($fc, $subs_uri, $offset = 0,$csv = '') {
         foreach($transaction_template['_embedded']['fx:items'] as $item) {
             $items[] = $item;
         }
-        $csv_header = '"' . $customer['id'] . '",';
+        $csv_header = '"' . $subscription['_links']["fx:sub_token_url"]["href"] . '",';
+        $csv_header .= '"' . $customer['id'] . '",';
         $csv_header .= '"' . $customer['email'] . '",';
         $csv_header .= '"' . $subscription['start_date'] . '",';
         $csv_header .= '"' . $subscription['next_transaction_date'] . '",';
@@ -44,29 +45,33 @@ function getSubscriptionsAsCSV($fc, $subs_uri, $offset = 0,$csv = '') {
         $csv_header .= '"' . $billing_address['postal_code'] . '",';
         $csv_header .= '"' . $billing_address['country'] . '",';
         foreach ($items as $item) {
-            $csv .= $csv_header;
-            $csv_detail = '';
-            $csv_detail .= '"' . $item['name'] . '",';
-            $csv_detail .= '"' . $item['price'] . '",';
-            $csv_detail .= '"' . $item['quantity'] . '",';
+            $item_price = $item['price'];
             $item_options = '';
             if (isset($item['_embedded']['fx:item_options'])) {
                 foreach ($item['_embedded']['fx:item_options'] as $item_option) {
-                    $item_options .= $item_option['name'] . ':' . $item_option['value'] . '|';
+                    $item_options .= str_replace('"', "'",$item_option['name'] . ':::' . $item_option['value'] . '|||');
+                    $item_price += $item_option['price_mod'];
                 }
             }
+            $csv .= $csv_header;
+            $csv_detail = '';
+            $csv_detail .= '"' . $item['name'] . '",';
+            $csv_detail .= '"' . $item_price . '",';
+            $csv_detail .= '"' . $item['quantity'] . '",';
             $item_options = trim($item_options,'|');
             $csv_detail .= '"' . $item_options . '",';
+            $shipping_address = '';
             foreach ($shipments as $shipment) {
-                if ($shipment['address_name'] == $item['shipto']) {
-                    $csv_detail .= '"' . $shipment['first_name'] . '",';
-                    $csv_detail .= '"' . $shipment['last_name'] . '",';
-                    $csv_detail .= '"' . $shipment['company'] . '",';
-                    $csv_detail .= '"' . $shipment['address1'] . '",';
-                    $csv_detail .= '"' . $shipment['city'] . '",';
-                    $csv_detail .= '"' . $shipment['region'] . '",';
-                    $csv_detail .= '"' . $shipment['postal_code'] . '",';
-                    $csv_detail .= '"' . $shipment['country'] . '",';
+                if ($shipment['address_name'] == $item['shipto'] && $shipping_address == '') {
+                    $shipping_address .= '"' . $shipment['first_name'] . '",';
+                    $shipping_address .= '"' . $shipment['last_name'] . '",';
+                    $shipping_address .= '"' . $shipment['company'] . '",';
+                    $shipping_address .= '"' . $shipment['address1'] . '",';
+                    $shipping_address .= '"' . $shipment['city'] . '",';
+                    $shipping_address .= '"' . $shipment['region'] . '",';
+                    $shipping_address .= '"' . $shipment['postal_code'] . '",';
+                    $shipping_address .= '"' . $shipment['country'] . '",';
+                    $csv_detail .= $shipping_address;
                 }
             }
             $csv_detail = trim($csv_detail,',');
@@ -74,27 +79,35 @@ function getSubscriptionsAsCSV($fc, $subs_uri, $offset = 0,$csv = '') {
             $csv .= "\n";
         }
     }
+    print $csv;
     if ($result['offset'] < $result['total_items'] && $result['returned_items'] == $result['limit']) {
         $offset += $result['limit'];
-        $csv .= getSubscriptionsAsCSV($fc, $subs_uri, $offset, $csv);
+        if ($offset > $result['total_items']) {
+            $offset = 0;
+        }
+        return $offset;
     }
-    return $csv;
+    return 0;
 }
 
 if ($action == 'view_subscriptions_as_csv') {
+    // in case you have a _lot_ of subscriptions
+    //ini_set('memory_limit', '2G');
     ?>
     <h2>View Subscriptions as CSV</h2>
-    <?php
-    $errors = array();
-    $subscriptions_uri = $_SESSION['subscriptions_uri'];
-    if (isset($_REQUEST['subscriptions_uri'])) {
-        $subscriptions_uri = $_REQUEST['subscriptions_uri'];
-    }
-    $csv = getSubscriptionsAsCSV($fc,$subscriptions_uri);
-    ?>
     <h3>Subscriptions for <?php print $_SESSION['store_name']; ?></h3>
 <pre>
-<?php print $csv; ?>
+<?php
+$subscriptions_uri = $_SESSION['subscriptions_uri'];
+if (isset($_REQUEST['subscriptions_uri'])) {
+    $subscriptions_uri = $_REQUEST['subscriptions_uri'];
+}
+$new_offset = printSubscriptionsAsCSV($fc,$subscriptions_uri);
+while($new_offset != 0) {
+sleep(1); // don't hammer the API
+$new_offset = printSubscriptionsAsCSV($fc,$subscriptions_uri,$new_offset);
+}
+?>
 </pre>
     <?php
 }
